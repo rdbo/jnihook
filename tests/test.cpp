c189418293538dda19d40308476bf436689b6cd8
rdbo <rdbodev@gmail.com>
2023-12-12 20:47:39 -0300

    fixed forced call in test

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,10 +1,9 @@
 #include <jnihook.h>
-#include <jvmti.h>
 #include <iostream>
 
 static JavaVM *jvm;
-static jvmtiEnv *jvmti;
 static int callCounter = 0;
+static jclass dummyClass;
 static jmethodID myFunctionID;
 
 jvalue hkMyFunction(jvalue *args, size_t nargs, void *thread, void *arg)
@@ -31,23 +30,19 @@ jvalue hkMyFunction(jvalue *args, size_t nargs, void *thread, void *arg)
 	jvalue name = args[1];
 	std::cout << "[*] mynumber value: " << mynumber.i << std::endl;
 	std::cout << "[*] name object: " << name.l << std::endl;
-	mynumber.i = 1337;
 
 	// Force call the original function
-	// jclass clazz;
-	// jvmti->GetMethodDeclaringClass(mID, &clazz);
-	// std::cout << "[*] clazz: " << clazz << std::endl;
+	mynumber.i = 1337;
+	jstring newName = jni->NewStringUTF("root");
 	
-	// jint result = jni->CallStaticIntMethod(clazz, mID, 1000, name->l);
-	// std::cout << "[*] Forced call result: " << result << std::endl;
+	jint result = jni->CallStaticIntMethod(dummyClass, myFunctionID, mynumber, newName);
+	std::cout << "[*] Forced call result: " << result << std::endl;
 
 	return jvalue { .i = 6969 };
 }
 
 static void start(JavaVM *jvm, JNIEnv *jni)
 {
-	jclass dummyClass;
-	
 	dummyClass = jni->FindClass("dummy/Dummy");
 	if (!dummyClass) {
 		std::cout << "[!] Failed to find dummy class" << std::endl;
@@ -86,11 +81,6 @@ dl_entry()
 	}
 
 	std::cout << "[*] JNIEnv: " << jni << std::endl;
-
-	if (jvm->GetEnv((void **)&jvmti, JVMTI_VERSION_1_0) != JNI_OK) {
-		std::cout << "[!] Failed to retrieve JVMTI environment" << std::endl;
-		return;
-	}
 
 	start(jvm, jni);
 }
