079e9ce1c5ba8cb4443f839d7fdec5611de0737b
rdbo <rdbodev@gmail.com>
2024-10-16 18:51:26 -0300

    updated tests with an example that doesnt work

--- tests/dummy/Dummy.java
+++ tests/dummy/Dummy.java
@@ -1,120 +1,25 @@
 package dummy;
 
-import jdk.jfr.Description;
+import java.io.IOException;
 
-interface Thing {
-    void doNothing();
-}
-
-class AnotherClass {
-    int number;
-
-    public int getNumber() {
-        System.out.println("-> ORIGINAL GET NUMBER CALLED");
-    	return this.number;
+class Target {
+    public static Target newTarget() {
+        return new Target();
     }
 
-    public void setNumber(int number) {
-        System.out.println("-> ORIGINAL SET NUMBER CALLED");
-    	this.number = number;
-    }
-
-    public void alsoDoNothing() {
-          
+    public void sayHello() {
+        System.out.println("Hello from Target object!");
     }
 }
 
-class YetAnotherClass {
-    public static YetAnotherClass instance = new YetAnotherClass(10);
+public class Dummy {
+    public static void main(String[] args) throws IOException {
+        System.out.print("Press [ENTER] to begin running...");
+        System.out.flush();
+        System.in.read();
 
-    private int secretNumber;
-
-    private ChildClass childObj;
-
-    public YetAnotherClass(int secretNumber) {
-        this.secretNumber = secretNumber;
-        this.childObj = new ChildClass();
-    }
-
-    public YetAnotherClass(YetAnotherClass obj) {
-        this.secretNumber = obj.secretNumber;
-        this.childObj = new ChildClass();
-        this.childObj.someLetter = obj.childObj.someLetter;
-    }
-
-    public static YetAnotherClass getInstance() {
-        return instance;
-    }
-
-    public int getSecretNumber() {
-        return this.secretNumber;
-    }
-
-    public char getLetter() {
-        return this.childObj.someLetter;
-    }
-
-    public static YetAnotherClass copy() {
-        return new YetAnotherClass(getInstance());
-    }
-
-    class ChildClass {
-        char someLetter = 'A';
-    }
-}
-
-public class Dummy implements Thing {
-    private class InternalSecretDummy {
-        String secret = "this is super hidden!!!";
-    }
-
-    InternalSecretDummy superSecret;
-
-    @Description(value = "This is a field")
-    public static int myNumberField = 10;
-
-    public Dummy() {
-        System.out.println("Constructor called!!!");
-    }
-
-    public static void sayHello() {
-        Dummy d = new Dummy();
-        System.out.println("Hello!!! This is the original function!!!");
-    }
-
-    public static void sayHi() {
-        System.out.println("Hi!!! This is the original function!!!");
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Started Dummy Process");
-
-        sayHello();
-
-        if (args.length == 0) {
-            System.out.println("Missing library path!");
-        } else {
-            System.out.println("Loading library...");
-            System.load(args[0]);
-        }
-
-        Thread.sleep(1000);
-
-        AnotherClass obj = new AnotherClass();
-        obj.setNumber(10);
-        System.out.println("-> My Number: " + obj.getNumber());
-
-        YetAnotherClass yetAnotherObj = YetAnotherClass.copy();
-        System.out.println("=>> OMG!!!! The secret number is: " + yetAnotherObj.getSecretNumber());
-
-        while (true) {
-            sayHello();
-            sayHi();
-            Thread.sleep(1000);
-        }
-    }
-
-    public void doNothing() {
-        
+        // Target target = Target.newTarget();
+        Target target = Target.newTarget();
+        target.sayHello();
     }
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -4,65 +4,10 @@
 #include <thread>
 #include <chrono>
 
-jmethodID orig_sayHello;
-void JNICALL hk_Dummy_sayHello(JNIEnv *env, jclass clazz)
+jmethodID orig_Target_sayHello;
+void JNICALL hk_Target_sayHello(JNIEnv *jni, jobject obj)
 {
-	std::cout << "Dummy.sayHello hook called!" << std::endl;
-	std::cout << "JNIEnv: " << env << std::endl;
-	std::cout << "Class: " << clazz << std::endl;
-
-	std::cout << "Original 'sayHello': " << orig_sayHello << std::endl;
-
-	std::cout << "Calling original sayHello..." << std::endl;
-	env->CallStaticVoidMethod(clazz, orig_sayHello);
-	std::cout << "Called original sayHello!" << std::endl;
-}
-
-void JNICALL hk_Dummy_sayHi(JNIEnv *env, jclass clazz)
-{
-	std::cout << "Dummy.sayHi hook called!" << std::endl;
-	std::cout << "JNIEnv: " << env << std::endl;
-	std::cout << "Class: " << clazz << std::endl;
-}
-
-jclass another_clazz;
-jmethodID orig_AnotherClass_getNumber;
-jint JNICALL hk_AnotherClass_getNumber(JNIEnv *env, jobject obj)
-{
-	std::cout << "-> GETNUMBER " << std::endl;
-	std::cout << "-> OBJ: " << obj << std::endl;
-
-	std::cout << "-> Original getNumber: " << orig_AnotherClass_getNumber << std::endl;
-
-	// NOTE: You must call Nonvirtual method because otherwise Java
-	//       will attempt to call the method closest to the object,
-	//       resulting in a infinitely recursing call and stack overflow.
-	jint real_number = env->CallNonvirtualIntMethod(obj, another_clazz, orig_AnotherClass_getNumber);
-	std::cout << "-> Actual number: " << real_number << std::endl;
-
-	return 1337;
-}
-
-jmethodID orig_setNumber;
-void JNICALL hk_AnotherClass_setNumber(JNIEnv *env, jobject obj, jint number)
-{
-	std::cout << "-> SETNUMBER " << std::endl;
-	std::cout << "-> OBJ: " << obj << std::endl;
-
-	std::cout << "-> Original setNumber: " << orig_setNumber << std::endl;
-
-	std::cout << "-> Expected number: " << number << std::endl;
-
-	env->CallNonvirtualIntMethod(obj, another_clazz, orig_setNumber, 42);
-}
-
-jobject orig_yetAnotherObj;
-jobject yet_another_obj;
-jobject JNICALL hk_YetAnotherClass_getInstance(JNIEnv *env, jclass clazz)
-{
-	std::cout << "[*] YetAnotherClass.getInstance() hook called!!!!!!!" << std::endl;
-
-	return yet_another_obj;
+	std::cout << "Target::sayHello HOOK CALLED!" << std::endl;
 }
 
 void
@@ -71,16 +16,10 @@ start()
 	JavaVM *jvm;
 	JNIEnv *env;
 	jsize jvm_count;
-	jclass clazz;
-	jmethodID sayHello_mid;
-	jmethodID sayHi_mid;
-	jmethodID getNumber_mid;
-	jmethodID setNumber_mid;
-	jclass yet_another_clazz;
-	jmethodID init_mid;
-	jmethodID getInstance_mid;
-	jmethodID getSecretNumber_mid;
+	jclass Target_class;
+	jmethodID Target_sayHello_mid;
 
+	// Setup JVM
 	std::cout << "[*] Library loaded!" << std::endl;
 
 	if (JNI_GetCreatedJavaVMs(&jvm, 1, &jvm_count) != JNI_OK) {
@@ -95,85 +34,26 @@ start()
 		return;
 	}
 
+	// Get classes and methods
+	Target_class = env->FindClass("dummy/Target");
+	std::cout << "[*] Class dummy.Target: " << Target_class << std::endl;
+
+	Target_sayHello_mid = env->GetMethodID(Target_class, "sayHello", "()V");
+	std::cout << "[*] Target::sayHello: " << Target_sayHello_mid << std::endl;
+
+	// Place hooks
 	if (auto result = JNIHook_Init(jvm); result != JNIHOOK_OK) {
 		std::cerr << "[!] Failed to initialize JNIHook: " << result << std::endl;
 		goto DETACH;
 	}
 
-	clazz = env->FindClass("dummy/Dummy");
-	std::cout << "[*] Class dummy.Dummy: " << clazz << std::endl;
-
-	sayHello_mid = env->GetStaticMethodID(clazz, "sayHello", "()V");
-	std::cout << "[*] Dummy.sayHello: " << sayHello_mid << std::endl;
-
-	sayHi_mid = env->GetStaticMethodID(clazz, "sayHi", "()V");
-	std::cout << "[*] Dummy.sayHi: " << sayHi_mid << std::endl;
-
-	if (auto result = JNIHook_Attach(sayHello_mid, reinterpret_cast<void *>(hk_Dummy_sayHello), &orig_sayHello); result != JNIHOOK_OK) {
+	if (auto result = JNIHook_Attach(Target_sayHello_mid, reinterpret_cast<void *>(hk_Target_sayHello), &orig_Target_sayHello); result != JNIHOOK_OK) {
 		std::cerr << "[!] Failed to attach hook: " << result << std::endl;
 		goto DETACH;
 	}
 
-	if (auto result = JNIHook_Attach(sayHi_mid, reinterpret_cast<void *>(hk_Dummy_sayHi), nullptr); result != JNIHOOK_OK) {
-		std::cerr << "[!] Failed to attach hook: " << result << std::endl;
-		goto DETACH;
-	}
-
-	another_clazz = env->FindClass("dummy/AnotherClass");
-	std::cout << "[*] Class dummy.AnotherClass: " << another_clazz << std::endl;
-
-	getNumber_mid = env->GetMethodID(another_clazz, "getNumber", "()I");
-	std::cout << "[*] AnotherClass.getNumber: " << getNumber_mid << std::endl;
-
-	setNumber_mid = env->GetMethodID(another_clazz, "setNumber", "(I)V");
-	std::cout << "[*] AnotherClass.setNumber: " << setNumber_mid << std::endl;
-
-	JNIHook_Attach(getNumber_mid, reinterpret_cast<void *>(hk_AnotherClass_getNumber),
-		       &orig_AnotherClass_getNumber);
-	std::cout << "[*] Original AnotherClass.getNumber: " << orig_AnotherClass_getNumber << std::endl;
-
-	// No need to cast when using <jnihook.hpp>. Requires C++ >= 23
-	orig_setNumber = jnihook::attach(setNumber_mid, hk_AnotherClass_setNumber).value();
-
-	getNumber_mid = env->GetMethodID(another_clazz, "getNumber", "()I");
-	std::cout << "[*] AnotherClass.getNumber (post hook): " << getNumber_mid << std::endl;
-
-	// YetAnotherClass hook
-	yet_another_clazz = env->FindClass("dummy/YetAnotherClass");
-	std::cout << "[*] Class dummy.YetAnotherClass: " << yet_another_clazz << std::endl;
-
-	init_mid = env->GetMethodID(yet_another_clazz, "<init>", "(I)V");
-	std::cout << "[*] YetAnotherClass.<init>: " << init_mid << std::endl;
-
-	getSecretNumber_mid = env->GetMethodID(yet_another_clazz, "getSecretNumber", "()I");
-	std::cout << "[*] YetAnotherClass.getSecretNumber: " << getSecretNumber_mid << std::endl;
-
-	getInstance_mid = env->GetStaticMethodID(yet_another_clazz, "getInstance", "()Ldummy/YetAnotherClass;");
-	std::cout << "[*] YetAnotherClass.getInstance: " << getInstance_mid << std::endl;
-
-	yet_another_obj = env->NewObject(yet_another_clazz, init_mid, 1337);
-	std::cout << "[*] Yet another object: " << yet_another_obj << std::endl;
-
-	std::cout << "[*] YAO's Secret number: " << env->CallIntMethod(yet_another_obj, getSecretNumber_mid) << std::endl;
-
-	orig_yetAnotherObj = env->CallStaticObjectMethod(yet_another_clazz, getInstance_mid);
-	std::cout << "[*] Original yetAnotherObj: " << orig_yetAnotherObj << std::endl;
-
-	std::cout << "[*] Original YAO's Secret number: " << env->CallIntMethod(orig_yetAnotherObj, getSecretNumber_mid) << std::endl;
-
-	std::cout << "[*] JNIHook_Attach: " << JNIHook_Attach(getInstance_mid, reinterpret_cast<void *>(hk_YetAnotherClass_getInstance), nullptr) << std::endl;
-
-	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-
-	if (auto result = JNIHook_Detach(sayHi_mid); result != JNIHOOK_OK) {
-		std::cout << "[*] Failed to detach hook: " << result << std::endl;
-	}
-
-	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-
-	JNIHook_Shutdown();
-
-	std::cout << "[*] JNIHook has been shut down" << std::endl;
+	// JNIHook_Shutdown();
+	// std::cout << "[*] JNIHook has been shut down" << std::endl;
 
 DETACH:
 	jvm->DetachCurrentThread(); // NOTE: The JNIEnv must live until JNIHook_Shutdown() is called!
