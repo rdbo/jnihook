3b621a3787949b58428ad09357ba011375239210
rdbo <rdbodev@gmail.com>
2025-05-19 22:00:10 +0000

    tabs to spaces

--- src/uuid.cpp
+++ src/uuid.cpp
@@ -8,26 +8,26 @@
 std::string
 GenerateUuid()
 {
-	std::stringstream uuid;
+        std::stringstream uuid;
 
-	std::random_device rd;
-	std::mt19937 rng(rd());
-	std::uniform_int_distribution<> udist;
+        std::random_device rd;
+        std::mt19937 rng(rd());
+        std::uniform_int_distribution<> udist;
 
-	auto now = std::chrono::system_clock::now();
-	auto since_epoch = now.time_since_epoch();
-	auto nanos_since_epoch = std::chrono::duration_cast<std::chrono::nanoseconds>(since_epoch);
+        auto now = std::chrono::system_clock::now();
+        auto since_epoch = now.time_since_epoch();
+        auto nanos_since_epoch = std::chrono::duration_cast<std::chrono::nanoseconds>(since_epoch);
 
-	// TODO: Seed 'rng' differently. It doesn't really
-	//       make sense to seed RNG with time, and also
-	//       use timestamp afterwards.
-	rng.seed(static_cast<unsigned int>(nanos_since_epoch.count()));
+        // TODO: Seed 'rng' differently. It doesn't really
+        //       make sense to seed RNG with time, and also
+        //       use timestamp afterwards.
+        rng.seed(static_cast<unsigned int>(nanos_since_epoch.count()));
 
-	uuid << std::hex;
-	
-	uuid << udist(rng) << "_" << udist(rng) << "_" << nanos_since_epoch.count();
+        uuid << std::hex;
+        
+        uuid << udist(rng) << "_" << udist(rng) << "_" << nanos_since_epoch.count();
 
-	// TODO: Consider hashing the whole thing
+        // TODO: Consider hashing the whole thing
 
-	return uuid.str();
+        return uuid.str();
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -8,91 +8,91 @@ jclass Target_class;
 jmethodID orig_Target_sayHello;
 JNIEXPORT void JNICALL hk_Target_sayHello(JNIEnv *jni, jobject obj)
 {
-	std::cout << "Target::sayHello HOOK CALLED!" << std::endl;
-	std::cout << "Calling original method..." << std::endl;
-	jni->CallNonvirtualVoidMethod(obj, Target_class, orig_Target_sayHello);
+        std::cout << "Target::sayHello HOOK CALLED!" << std::endl;
+        std::cout << "Calling original method..." << std::endl;
+        jni->CallNonvirtualVoidMethod(obj, Target_class, orig_Target_sayHello);
 }
 
 void
 start()
 {
-	JavaVM *jvm;
-	JNIEnv *env;
-	jsize jvm_count;
-	jmethodID Target_sayHello_mid;
+        JavaVM *jvm;
+        JNIEnv *env;
+        jsize jvm_count;
+        jmethodID Target_sayHello_mid;
 
-	// Setup JVM
-	std::cout << "[*] Library loaded!" << std::endl;
+        // Setup JVM
+        std::cout << "[*] Library loaded!" << std::endl;
 
-	if (JNI_GetCreatedJavaVMs(&jvm, 1, &jvm_count) != JNI_OK) {
-		std::cerr << "[!] Failed to get created Java VMs!" << std::endl;
-		return;
-	}
+        if (JNI_GetCreatedJavaVMs(&jvm, 1, &jvm_count) != JNI_OK) {
+                std::cerr << "[!] Failed to get created Java VMs!" << std::endl;
+                return;
+        }
 
-	std::cout << "[*] JavaVM: " << jvm << std::endl;
+        std::cout << "[*] JavaVM: " << jvm << std::endl;
 
-	if (jvm->AttachCurrentThread(reinterpret_cast<void **>(&env), NULL) != JNI_OK) {
-		std::cerr << "[!] Failed to attach current thread to JVM!" << std::endl;
-		return;
-	}
+        if (jvm->AttachCurrentThread(reinterpret_cast<void **>(&env), NULL) != JNI_OK) {
+                std::cerr << "[!] Failed to attach current thread to JVM!" << std::endl;
+                return;
+        }
 
-	// Get classes and methods
-	Target_class = env->FindClass("dummy/Target");
-	std::cout << "[*] Class dummy.Target: " << Target_class << std::endl;
+        // Get classes and methods
+        Target_class = env->FindClass("dummy/Target");
+        std::cout << "[*] Class dummy.Target: " << Target_class << std::endl;
 
-	Target_sayHello_mid = env->GetMethodID(Target_class, "sayHello", "()V");
-	std::cout << "[*] Target::sayHello: " << Target_sayHello_mid << std::endl;
+        Target_sayHello_mid = env->GetMethodID(Target_class, "sayHello", "()V");
+        std::cout << "[*] Target::sayHello: " << Target_sayHello_mid << std::endl;
 
-	// Place hooks
-	if (auto result = JNIHook_Init(jvm); result != JNIHOOK_OK) {
-		std::cerr << "[!] Failed to initialize JNIHook: " << result << std::endl;
-		goto DETACH;
-	}
+        // Place hooks
+        if (auto result = JNIHook_Init(jvm); result != JNIHOOK_OK) {
+                std::cerr << "[!] Failed to initialize JNIHook: " << result << std::endl;
+                goto DETACH;
+        }
 
-	if (auto result = JNIHook_Attach(Target_sayHello_mid, reinterpret_cast<void *>(hk_Target_sayHello), &orig_Target_sayHello); result != JNIHOOK_OK) {
-		std::cerr << "[!] Failed to attach hook: " << result << std::endl;
-		goto DETACH;
-	}
+        if (auto result = JNIHook_Attach(Target_sayHello_mid, reinterpret_cast<void *>(hk_Target_sayHello), &orig_Target_sayHello); result != JNIHOOK_OK) {
+                std::cerr << "[!] Failed to attach hook: " << result << std::endl;
+                goto DETACH;
+        }
 
-	std::cout << "[*] Hooks attached" << std::endl;
+        std::cout << "[*] Hooks attached" << std::endl;
 
-	// JNIHook_Shutdown();
-	// std::cout << "[*] JNIHook has been shut down" << std::endl;
+        // JNIHook_Shutdown();
+        // std::cout << "[*] JNIHook has been shut down" << std::endl;
 
 DETACH:
-	jvm->DetachCurrentThread(); // NOTE: The JNIEnv must live until JNIHook_Shutdown() is called!
-				    //       (or if you won't call JNIHook again).
+        jvm->DetachCurrentThread(); // NOTE: The JNIEnv must live until JNIHook_Shutdown() is called!
+                                    //       (or if you won't call JNIHook again).
 }
 
 #ifdef _WIN32
 #include <windows.h>
 DWORD WINAPI WinThread(LPVOID lpParameter)
 {
-	start();
-	return 0;
+        start();
+        return 0;
 }
 
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
-	switch (dwReason) {
-	case DLL_PROCESS_ATTACH:
-		CreateThread(nullptr, 0, WinThread, nullptr, 0, nullptr);
-		break;
-	}
-	
-	return TRUE;
+        switch (dwReason) {
+        case DLL_PROCESS_ATTACH:
+                CreateThread(nullptr, 0, WinThread, nullptr, 0, nullptr);
+                break;
+        }
+        
+        return TRUE;
 }
 #else
 void *main_thread(void *arg)
 {
-	start();
-	return NULL;
+        start();
+        return NULL;
 }
 
 void __attribute__((constructor))
 dl_entry()
 {
-	pthread_t th;
-	pthread_create(&th, NULL, main_thread, NULL);
+        pthread_t th;
+        pthread_create(&th, NULL, main_thread, NULL);
 }
 #endif
