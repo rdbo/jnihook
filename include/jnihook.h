773a476680038fc79c5f919defaf83b3c1c0d905
rdbo <rdbodev@gmail.com>
2026-03-03 21:36:34 +0000

    update file headers

--- include/jnihook.h
+++ include/jnihook.h
@@ -6,7 +6,7 @@
  */
 
 /*
- * Copyright (C) 2023    Rdbo
+ * Copyright (C) 2026    Rdbo
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License version 3
  * as published by the Free Software Foundation.
